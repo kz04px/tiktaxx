10cd18954da1c66e4bb6eecc1ce1caf8336d63e5
Unknown <Twipply@gmail.com>
2018-04-20 22:23:59 +0100

    Correctly score boards
    394: 200 - 178 - 16
    Elo: +19.42 +/- 33.58
    LOS: 87.1%

--- src/score.cpp
+++ src/score.cpp
@@ -1,12 +1,32 @@
 #include "score.hpp"
+#include "bitboards.hpp"
 #include "other.hpp"
 
 int score(const Position &pos)
 {
+    uint64_t empty = U64_BOARD & ~(pos.pieces[PIECE::CROSS] | pos.pieces[PIECE::NOUGHT] | pos.blockers);
+
     int num_cross = popcountll(pos.pieces[PIECE::CROSS]);
     int num_nought = popcountll(pos.pieces[PIECE::NOUGHT]);
     int num_blockers = popcountll(pos.blockers);
-    int num_empty = 49 - (num_cross + num_nought + num_blockers);
+    int num_empty = popcountll(empty);
+
+    uint64_t cross_moves = single_jump(pos.pieces[PIECE::CROSS]);
+    cross_moves = single_jump(cross_moves);
+    cross_moves &= empty;
+
+    uint64_t nought_moves = single_jump(pos.pieces[PIECE::NOUGHT]);
+    nought_moves = single_jump(nought_moves);
+    nought_moves &= empty;
+
+    if(cross_moves == 0ULL && nought_moves != 0ULL)
+    {
+        num_nought += num_empty;
+    }
+    else if(cross_moves != 0ULL && nought_moves == 0ULL)
+    {
+        num_cross += num_empty;
+    }
 
     int score = num_cross - num_nought;
 
